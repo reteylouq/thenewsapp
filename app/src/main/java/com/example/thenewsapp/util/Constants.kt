02da00413c2b5abfa30fa872003bfb881d4524bc
reteylouq <artiomushka000@gmail.com>
2024-06-13 22:01:36 +0300

    MAIN

---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "3c5b6eaa03864af4b97680f800620c8f"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
